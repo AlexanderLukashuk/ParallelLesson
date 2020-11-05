e797bd2ef1b911dc6b8b25ee76b53f52deabc7fa
AlexanderLukashuk <upiter465@gmail.com>
2020-11-05 20:55:39 +0600

    Parallel Lesson, AsParallel()

---
+++ ParallelLesson/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParallelLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Parallel.Invoke(() =>
+            //{
+            //    Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            //}, () =>
+            //{
+            //    Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            //}, () =>
+            //{
+            //    Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            //}, () =>
+            //{
+            //    Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            //}, () =>
+            //{
+            //    Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            //}, () =>
+            //{
+            //    Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            //}, () =>
+            //{
+            //    Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            //}, () =>
+            //{
+            //    Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            //}, () =>
+            //{
+            //    Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            //});
+
+
+            //Parallel.For(0, 10, (i) =>
+            //{
+            //    Console.WriteLine(i);
+            //});
+
+            //Parallel.ForEach(new List<string> { "Astana", "Almaty", "Kostanay" }, (cityName) =>
+            //{
+            //    Console.WriteLine(cityName);
+            //});
+
+            var list = new List<string>
+            {
+                "Astana", "Almaty", "Kostanay", "Moscow", "Grozniy"
+            };
+
+            var result = list.AsParallel()
+                .AsOrdered()
+                .Where(cityName =>
+                {
+                    Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} - {cityName} - {DateTime.Now.Ticks}");
+                    return cityName.Contains("a");
+                })
+                .ToList();
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
